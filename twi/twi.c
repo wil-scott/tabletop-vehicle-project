bbae7a27869542f37fbc1e461bfd38a2ab883763
wil-scott <wscott53@my.bcit.ca>
2023-08-13 04:28:30 +0000

    Move i2c write logic into public function

--- twi/twi.c
+++ twi/twi.c
@@ -78,15 +78,46 @@ void TWI_Stop(void)
 /*
  * WRITE to address
  * currently waits for WIF flag (either from MADDR set, or previous MDATA write)
- * Future versions will likely rely on interrupt/ISR
+ * Future versions will likely rely on interrupt/ISR (move away from polling)
  */
 void TWI_Write(uint8_t data)
 {
+	if (sizeof(data) == 1) { //if errors, blink and continue
+		PORTE.OUT |= PIN3_bm;
+	}
 	while (!(TWI0.MSTATUS & WIF)) {	}
+/*	if ((TWI0.MSTATUS & 0x10) == 1) {*/
+/*		PORTE.OUT |= PIN2_bm;*/
+/*	}*/
+/*	if ((TWI0.MSTATUS & (ARBLOST|BUSERR))){*/
+/*		PORTE.OUT |= PIN2_bm;*/
+/*	}*/
 	TWI0.MDATA = data;
 
-	
 }
+
+/*****************************************************************
+ * 
+ * Public Functions
+ *
+ *****************************************************************/
+uint8_t i2c_write(volatile uint8_t* data_buffer, int data_size)
+{
+	TWI_Start();
+	TWI_interrupt_init();
+
+	for (int i = 0; i < data_size; i++)
+	{
+		TWI_Write(data_buffer[i]);
+	}
+	_delay_ms(25);
+	TWI_Stop();
+	return 0x00;
+}
+
+//uint8_t config_buff[] = { 0x00, 0xA8, 0x3F, 0xD3, 0x00, 0x40, 0xA1, 0xC0, 0xDA, 0x02, 0x81, 0x7F, 0xA4, 0xA6, 0xD5, 0x80, 0x8D, 0x14, 0xAF };
+static const uint8_t *start;
+static const uint8_t *end;
 
 int main(void)
 {
@@ -105,58 +136,16 @@ int main(void)
 	
 	//init TWI
 	TWI_Init();
-	TWI_Start();
-	TWI_interrupt_init();
+	//TWI_Start();
+	//TWI_interrupt_init();
 	
-	//blink onboard led if TWI owns the Bus
-	if ((TWI0.MSTATUS & OWNER) == 0x02) {
-		for (int x = 0; x < 5; x++)
-		{
-			_delay_ms(10);
-			PORTE.OUT |= PIN2_bm;
-			_delay_ms(10);
-			PORTE.OUT &= ~PIN2_bm;
-		}
-		
-	} else { return 0; } //if not owner of bus, exit
-	
-	//Write command byte stream message to OLED
-	TWI_Write(0x00);
-	if ((TWI0.MSTATUS & 0x1C) == 0x00) { //if no errors, blink and continue
-		PORTE.OUT |= PIN3_bm;
-	}
-	
-	TWI_Write(0xA8); //write first command to OLED
-	PORTE.OUT &= ~PIN2_bm; // turn off on-board LED
+	static volatile uint8_t config_buff[19] = { 0x00, 0xA8, 0x3F, 0xD3, 0x00, 0x40, 0xA1, 0xC0, 0xDA, 0x02, 0x81, 0x7F, 0xA4, 0xA6, 0xD5, 0x80, 0x8D, 0x14, 0xAF };
 
-	if ((TWI0.MSTATUS & 0x1C)== 0x00 ) { //if no error flags, blink and continue
-
-		PORTA.OUT |= PIN1_bm;
-
-	}
+	uint8_t ret_val = i2c_write(config_buff, 19);
 	
-	// general config commands from SDD1306 data sheet for oled setup
-	TWI_Write(0x3F);
-	TWI_Write(0xD3);
-	TWI_Write(0x00);
-	TWI_Write(0x40);
-	TWI_Write(0xA1);
-	TWI_Write(0xC0);
-	TWI_Write(0xDA);
-	TWI_Write(0x02);
-	TWI_Write(0x81);
-	TWI_Write(0x7F);
-	TWI_Write(0xA4);
-	TWI_Write(0xA6);
-	TWI_Write(0xD5);
-	TWI_Write(0x80);
-	TWI_Write(0x8D);
-	TWI_Write(0x14);
-	TWI_Write(0xAF);
-	
-	_delay_ms(25); // need to wait for WIF flag after last write command
-	TWI_Stop();
-	_delay_ms(25);
+	//_delay_ms(25); // need to wait for WIF flag after last write command
+	//TWI_Stop();
+
 	// turn off LED to confirm finished/not crashed
 	if (TWI0.MSTATUS == 0x1) {
 		PORTE.OUT &= ~PIN3_bm;
@@ -167,9 +156,27 @@ int main(void)
 }
 
 
-
-
-
+/*	}*/
+	// general config commands from SDD1306 data sheet for oled setup
+/*	TWI_Write(0x00);*/
+/*	TWI_Write(0xA8);*/
+/*	TWI_Write(0x3F);*/
+/*	TWI_Write(0xD3);*/
+/*	TWI_Write(0x00);*/
+/*	TWI_Write(0x40);*/
+/*	TWI_Write(0xA1);*/
+/*	TWI_Write(0xC0);*/
+/*	TWI_Write(0xDA);*/
+/*	TWI_Write(0x02);*/
+/*	TWI_Write(0x81);*/
+/*	TWI_Write(0x7F);*/
+/*	TWI_Write(0xA4);*/
+/*	TWI_Write(0xA6);*/
+/*	TWI_Write(0xD5);*/
+/*	TWI_Write(0x80);*/
+/*	TWI_Write(0x8D);*/
+/*	TWI_Write(0x14);*/
+/*	TWI_Write(0xAF);*/
 
 
 
