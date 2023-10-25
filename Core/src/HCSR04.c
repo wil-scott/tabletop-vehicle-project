57de2b8ee9e49eaa355895756aefb48828687b0e
wil-scott <wscott53@my.bcit.ca>
2023-10-25 18:22:47 +0000

    Refactor timer code to use ICP1 pin for rise/fall timing with results accurate to ~.5cm

--- Core/src/HCSR04.c
+++ Core/src/HCSR04.c
@@ -7,117 +7,71 @@
 
 #include "../inc/HCSR04.h"
 
-// Create global var to track PortDPin3 State
-// if prev state low (now high), set and start timer
-// if prev state high (now low), set and end timer
-volatile int echo_pin_state = 0;
-volatile int timeout = 0;
-volatile int flag = 0;
+#define TIMER1_START_VAL	0
+#define TRIGGER_PIN			2
+#define ECHO_PIN			3
+
+volatile uint16_t timer1_compare_match = 9501;
 volatile uint16_t end_time;
+volatile uint8_t flag = 0;
+volatile uint8_t timeout = 0;
+volatile uint8_t pin_state = 0;
 
-const uint16_t timer1_start_val = 0;
-const uint16_t timer1_compare_match = 9501;
-const int speed_of_sound = 343;
-
-/*
- * Configure ATMEGA328p pins for r/w, interrupts
- */
-
-void pin_config()
+void init_pins()
 {
 	//turn on HC-SR04 pins
 	DDRD |= (1<<PIND2); //output for trigger
-	DDRD &= ~(1<<PIND3); //input for echo
-	
-	//enable PCIE2 Bit2=1 for Port D
-	PCICR |= 0x4 ; //0b00000100 or PCIE2
-	//ENABLE PCINT19 (PortD Pin 3)
-	PCMSK2 |= 0x8; //0b00001000 or PCINT19
-		
+	DDRB &= ~(1<<PINB0); //input for echo	
 }
 
-void timer_config()
+void init_timer1()
 {
-	//16MHz clock by default
-	//1 clock tick = 62.5ns
-	//prescale 16MHz by 64 to 250Khz gives us 4us per tick
-	//38000/4 = 9500
-	
-	//16000000 / (64 x 9500 + 1) = 38ms
-	
 	//init Timer1
 	TCCR1A = 0;
 	TCCR1B = 0;
 	
-	//preload timer with comp match value
-	//TCNT1 = 0;
-	
-	//set prescaler to 64
-	TCCR1B |= (1 << CS11);
-	TCCR1B |= (1 << CS10);
-
+	TCNT1 = 0;
+	TCCR1B = (1 << ICES1); //capture rising edge
+	TCCR1B |= 0x3; //set prescaler to 64
+	TIMSK1 = (1 << ICIE1) | (1 << OCIE1A); // enable inp capt and comp int
 }
 
-/*
- * Sets the trigger pin low (clears it) for 2us, then
- * high for the 10us required to trigger the sensor's ping
- */
 void trigger()
 {
 	PORTD &= ~(1 << PIND2);
 	_delay_us(2);
 	PORTD |= (1 << PIND2);
 	_delay_us(10);
-	PORTD &= ~(1 << PIND2);
+	PORTD &= ~(1 << PIND2);	
 }
 
 int measure()
 {
 	// clear interrupts
 	cli();
-	pin_config();
-	timer_config();
+	init_pins();
+	init_timer1();
 	sei();
-	echo_pin_state = 0;
-	timeout = 0;
-	flag = 0;
 	trigger();
-
+	
 	while(!flag) {}
-
-	if(timeout) {
-		return -1;
-	}
-	//convert counter val from ticks to us
-	int microseconds = end_time * 4;
-	int distance_mm = (microseconds/2) * speed_of_sound / 1000;
-	cli();
 	
-	return microseconds;
-	
-	
+	return end_time;
 }
 
-ISR(PCINT2_vect)
+ISR(TIMER1_CAPT_vect)
 {
-	//DDRB |= (1 << DDB1);
-   // PORTB |= (1 << PINB1);
-	//ISR for Port D
-	//if pin was low before state change
-	if (echo_pin_state == 0) {
-
-		//set pin to current state (high)
-		echo_pin_state = 1;
-		//RESET timer
-		TCNT1 = timer1_start_val;
-		OCR1A = timer1_compare_match;
-		//Enable timer interrupt for compare mode
-		TIMSK1 |= (1 << OCIE1A);
-	} else { //if pin is high before state change
-		end_time = TCNT1;
+	if (pin_state == 0) {
+		pin_state = 1;
+		TCNT1 = TIMER1_START_VAL; // set timer to 0
+		OCR1A = timer1_compare_match;	// set compare val
+		TCCR1B &= ~(1 << ICES1); // set interrupt to falling edge
+		
+	} else {
+		end_time = ICR1;
 		flag = 1;
 	}
-	//PCIFR should be auto reset when ISR triggered
+
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -125,6 +79,3 @@ ISR(TIMER1_COMPA_vect)
 	//ISR for Compare Match of Timer1
 	timeout = 1;
 }
-
-
-
